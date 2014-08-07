79b3a87a8a360058a10ddd3605bbc4ac56f1baeb
Jim Garrison <jim@garrison.cc>
2014-08-06 21:23:22 -0700

    initial commit

---
+++ README.rst
@@ -0,0 +1,18 @@
+toml2json
+=========
+
+I needed a script I could run from the commandline to convert toml_ to
+json_ (and vice versa).
+
+This script is written in python, and depends on the `toml package
+<https://github.com/uiri/toml>`_
+
+Because ``datetime``'s are not supported in JSON, they do not make the
+roundtrip properly.  After the toml => json => toml conversion, each
+``datetime`` will be a string, just like when using the web service
+http://toml-to-json.matiaskorhonen.fi/
+
+More to come.
+
+.. _toml: https://github.com/toml-lang/toml
+.. _json: http://json.org/
---
+++ toml2json.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import datetime
+import json
+import sys
+
+import toml
+
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            # http://stackoverflow.com/q/19654578/1558890
+            return obj.isoformat() + "Z"
+        return super(CustomEncoder, self).default(obj)
+
+
+def toml2json(s):
+    return json.dumps(toml.loads(s), cls=CustomEncoder)
+
+
+def json2toml(s):
+    return toml.dumps(json.loads(s))
+
+
+def main():
+    if "toml2json" in sys.argv[0]:
+        func = toml2json
+    elif "json2toml" in sys.argv[0]:
+        func = json2toml
+    else:
+        raise RuntimeError("Must be called either as `toml2json` or `json2toml`.")
+
+    if not len(sys.argv) == 2:
+        # FIXME
+        raise RuntimeError("Must be called with a filename as argument.")
+
+    with open(sys.argv[1]) as f:
+        s = f.read()
+    result = func(s)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
